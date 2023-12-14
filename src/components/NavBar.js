65b7d2ff1214f89799b0a9498664d168afa73968
SalmanDanesh93 <salmand64@Yahoo.com>
2023-12-13 22:13:33 -0800

    updated NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,3 +3,27 @@ import { NavBar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/sullyscreative.png";
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router} from "react-router-dom";
+
+export const NavBar = () => {
+
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScroll] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return ()
+}
